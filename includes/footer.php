7313bcdd384904de5e673a7c31c37322f6df3674
Raisrosdi <raisrosdi090917@gmail.com>
2025-02-09 12:39:17 +0800

    First Commit
    First Date: 08-02-2025

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Shaklee Employee Leave Management System in PHP | Developed By <b>Rais Rosdi, Elia, Amni</b> </p>
+        </div>
+</footer>
